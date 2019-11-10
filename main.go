a44e2fd577199f4054c425aa766759ead11139a6
Mimoja <git@mimoja.de>
2019-11-10 09:22:02 +0100

    Remove worker and handle analysis in sync

--- main.go
+++ main.go
@@ -8,17 +8,6 @@ import (
 
 var Bundle MFTCommon.AppBundle
 
-const NumberOfWorker = 1
-
-func worker(id int, file <-chan MFTCommon.DownloadEntry) {
-
-	for true {
-		entry := <-file
-		Bundle.Log.WithField("file", entry).Infof("Handeling %s in Worker %d", entry.PackageID.GetID(), id)
-		analyse(entry)
-	}
-}
-
 func main() {
 
 	Bundle = MFTCommon.Init("Importer")
@@ -28,10 +17,6 @@ func main() {
 
 	setupYara()
 
-	entries := make(chan MFTCommon.DownloadEntry, NumberOfWorker)
-	for w := 1; w <= NumberOfWorker; w++ {
-		go worker(w, entries)
-	}
 
 	Bundle.MessageQueue.DownloadedQueue.RegisterCallback("Importer", func(payload string) error {
 
@@ -42,9 +27,8 @@ func main() {
 			Bundle.Log.WithField("payload", payload).WithError(err).Error("Could not unmarshall json: %v", err)
 		}
 
-		//go analyse(entry)
-		//analyse(entry)
-		entries <- entry
+		Bundle.Log.WithField("file", entry).Infof("Handeling %s in Worker %d", entry.PackageID.GetID(), id)
+		analyse(entry)
 		return nil
 	})
 	Bundle.Log.Infof("Starting up!")
